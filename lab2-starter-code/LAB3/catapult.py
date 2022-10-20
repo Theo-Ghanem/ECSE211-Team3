b9a1e5cd4623eb0715525df5c5709de1f7309afa
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-10-20 19:11:35 -0400

    made it come back to inital postion

--- lab2-starter-code/LAB3/catapult.py
+++ lab2-starter-code/LAB3/catapult.py
@@ -16,3 +16,10 @@ while motor_left.is_moving():
     time.sleep(0.1)
 print("motor_left.set_position_relative(-180)")
 input("Press any key to continue...")
+
+# command to rotate 60deg away from current position
+motor_left.set_position_relative(180)
+while motor_left.is_moving():
+    time.sleep(0.1)
+print("motor_left.set_position_relative(180)")
+input("Press any key to continue...")
