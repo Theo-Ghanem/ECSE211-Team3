d50e2225871ae1382bd3373a5bafd7b6a635d5c3
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-10-20 18:56:48 -0400

    changed code for catapult

---
+++ lab2-starter-code/LAB3/catapult.py
@@ -0,0 +1,18 @@
+from utils.brick import Motor
+import time
+
+motor_left = Motor("A")
+
+# Set target speed first, 360 deg/sec
+# Reset power limit to limitless with 0, default values:(power=0, dps=0)
+motor_left.set_limits(dps=360)
+
+# set current position to absolute pos 0deg
+motor_left.reset_encoder()
+
+# command to rotate 60deg away from current position
+motor_left.set_position_relative(60)
+while motor_left.is_moving():
+    time.sleep(0.1)
+print("motor_left.set_position_relative(60)")
+input("Press any key to continue...")
