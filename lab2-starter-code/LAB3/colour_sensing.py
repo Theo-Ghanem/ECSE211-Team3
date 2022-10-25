d6c4fe45cbea528ef176c2c52b743dc6eef33df5
sabrina0822 <90227881+sabrina0822@users.noreply.github.com>
2022-10-25 17:31:56 -0400

    Normalize input data point for color sensing

--- lab2-starter-code/LAB3/colour_sensing.py
+++ lab2-starter-code/LAB3/colour_sensing.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from this import d
 from utils.brick import Motor, TouchSensor, wait_ready_sensors
 from time import sleep
 import math
@@ -9,11 +10,16 @@ colors = (("red",(0.792319061, 0.09501644, 0.112664499)),("green",(0.145115189, 
 async def play_note(color_sensor):
     sleep(1)
     print("doing things")
-    data_point = color_sensor.get_value()
+    data_point = normalizePoint(color_sensor.get_value())
     closest_color = distance(data_point)
     print(data_point,closest_color)
     return None
 
+
+def normalizePoint(data_point): 
+    added_colors = data_point[0] + data_point[1] + data_point[2]
+    for i in data_point: 
+        data_point[i] = data_point[1]/added_colors 
 
 
 def getColorDistance(color,target):
