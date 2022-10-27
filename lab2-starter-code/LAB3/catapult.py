9c22c8268148c9706acaf7612d4fcab7200e7b81
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-10-27 12:01:58 -0400

    resetting

--- lab2-starter-code/LAB3/catapult.py
+++ lab2-starter-code/LAB3/catapult.py
@@ -3,22 +3,27 @@
 from utils.brick import Motor, TouchSensor, wait_ready_sensors
 from time import sleep
 
-async def launch_cube(motor_left):
+
+def launch_cube(motor_left):
     # command to rotate 80deg away from current position
     motor_left.set_limits(dps=0)
     motor_left.set_position_relative(-80)
+    # motor_left.wait_is_moving()
     while motor_left.is_moving():
         sleep(0.1)
     print("motor_left.set_position_relative(-80)")
 
+    print("1")
     sleep(1)
+    print("2")
     # command to rotate 80deg away from current position
     motor_left.set_limits(dps=360)
     motor_left.set_position_relative(80)
     while motor_left.is_moving():
         sleep(0.1)
+    # motor_left.wait_is_moving()
     print("motor_left.set_position_relative(80)")
 
-    sleep(1)
+    sleep(0.1)
     motor_left.set_power(0)  # always do 0% to stop motor
     print("motor_left.set_power(0)")
--- lab2-starter-code/LAB3/main.py
+++ lab2-starter-code/LAB3/main.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 from utils.sound import Sound
 from colour_sensing import play_note
-from utils.brick import Motor, TouchSensor, EV3ColorSensor, wait_ready_sensors 
+from utils.brick import Motor, TouchSensor, EV3ColorSensor, wait_ready_sensors
 from time import sleep
 from catapult import *
 from drumming import *
@@ -21,15 +21,16 @@ motor_left.reset_encoder()
 ts_colour = TouchSensor(1)
 ts_drums = TouchSensor(2)
 ts_stop = TouchSensor(3)
-drumTone = Sound(duration=0.1, volume=80, pitch="C2")
-tone1 = Sound(duration=0.1, volume=100, pitch="C4")
-tone2 = Sound(duration=0.1, volume=100, pitch="D4")
-tone3 = Sound(duration=0.1, volume=100, pitch="E4")
-tone4 = Sound(duration=0.1, volume=100, pitch="G4")
+drumSound = Sound(duration=0.5, volume=70, pitch="F5")
+tone1 = Sound(duration=0.5, volume=80, pitch="C4")
+tone2 = Sound(duration=0.5, volume=80, pitch="D4")
+tone3 = Sound(duration=0.5, volume=80, pitch="E4")
+tone4 = Sound(duration=0.5, volume=80, pitch="G4")
 
 color_sensor = EV3ColorSensor(4)
 
 wait_ready_sensors(True)
+
 
 async def read_button_colour(touch_sensor, motor_left, color_sensor):
     try:
@@ -39,8 +40,10 @@ async def read_button_colour(touch_sensor, motor_left, color_sensor):
             if touch_sensor.is_pressed() and not running:
                 print("Colour button pressed")
                 # Read colour, wait till done then launch cube
-                await play_note(color_sensor, tone1, tone2, tone3, tone4)
-                await launch_cube(motor_left)
+                play_note(color_sensor, tone1, tone2, tone3, tone4)
+                print("done")
+                launch_cube(motor_left)
+                print("launched")
                 running = True
             elif not touch_sensor.is_pressed():
                 running = False
@@ -48,17 +51,19 @@ async def read_button_colour(touch_sensor, motor_left, color_sensor):
     except BaseException:
         exit()
 
+
 async def read_button_drums(touch_sensor):
     try:
         while True:
             sleep(0.01)
             if touch_sensor.is_pressed():
                 print("Drum button pressed")
-                start_drum(drumTone)
+                start_drum(drumSound)
 
     # capture all exceptions including KeyboardInterrupt (Ctrl-C)
     except BaseException:
         exit()
+
 
 async def read_button_stop(touch_sensor):
     try:
@@ -72,16 +77,18 @@ async def read_button_stop(touch_sensor):
         exit()
 
 
-
 if __name__ == '__main__':
     try:
         print("Starting threads")
         kill_threads = False
-        colour_thread = Thread(target=asyncio.run, args=(read_button_colour(ts_colour, motor_left, color_sensor),))
+        colour_thread = Thread(target=asyncio.run, args=(
+            read_button_colour(ts_colour, motor_left, color_sensor),))
         colour_thread.daemon = True
-        drum_thread = Thread(target=asyncio.run, args=(read_button_drums(ts_drums),))
+        drum_thread = Thread(target=asyncio.run, args=(
+            read_button_drums(ts_drums),))
         drum_thread.daemon = True
-        stop_thread = Thread(target=asyncio.run, args=(read_button_stop(ts_stop),))
+        stop_thread = Thread(target=asyncio.run,
+                             args=(read_button_stop(ts_stop),))
         stop_thread.daemon = True
 
         colour_thread.start()
@@ -93,7 +100,3 @@ if __name__ == '__main__':
         exit()
     except BaseException:
         exit()
-
-
-
-
