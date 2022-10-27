13c6e1e96fea8a5f186ee63552e9771c76e585f7
sabrina0822 <90227881+sabrina0822@users.noreply.github.com>
2022-10-26 23:36:31 -0400

    changed from sleep to smthg else idk

--- lab2-starter-code/LAB3/catapult.py
+++ lab2-starter-code/LAB3/catapult.py
@@ -7,8 +7,8 @@ async def launch_cube(motor_left):
     # command to rotate 80deg away from current position
     motor_left.set_limits(dps=0)
     motor_left.set_position_relative(-80)
-    while motor_left.is_moving():
-        sleep(0.1)
+    motor_left.wait_is_moving()
+        #sleep(0.1)
     print("motor_left.set_position_relative(-80)")
 
     print("1")
@@ -17,8 +17,9 @@ async def launch_cube(motor_left):
     # command to rotate 80deg away from current position
     motor_left.set_limits(dps=360)
     motor_left.set_position_relative(80)
-    while motor_left.is_moving():
-        sleep(0.1)
+    # while motor_left.is_moving():
+    #     sleep(0.1)
+    motor_left.wait_is_moving()
     print("motor_left.set_position_relative(80)")
 
     sleep(0.1)
