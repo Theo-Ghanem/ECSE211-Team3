7dc70875c9facac5c659d0b4a8c3fd89ad150b10
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-11-12 12:18:20 -0500

    integrated extender with main

--- finalproject/project/extender.py
+++ finalproject/project/extender.py
@@ -3,39 +3,43 @@
 from utils.brick import Motor, TouchSensor, wait_ready_sensors
 from time import sleep
 
-motor_row = Motor("A")  # remove this after
-motor_column = Motor("B")  # remove this after
+# motor_row = Motor("A")  # remove this after
+# motor_column = Motor("B")  # remove this after
 
 # constants for row distance:
 row_distance = [140, 110, 90, 80, 60]
 column_distance = [140, 110, 90, 80, 60]  # probably will be different than row
 
-grid = [
-    [1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1]
-]
+# grid = [
+#     [1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1]
+# ]
 
 
-def pushRow(motor_row, grid, iteration):
+def pushRow(motor_row, motor_column, grid, iteration, verbose):
 
     motor_row.set_limits(dps=80)  # speed of motor
     # make sure the motor is in correct position at start!
     motorStartPosition = motor_row.get_position()
 
     counter = 0
+    atLeast1Cube = False
     for i in grid[iteration]:
         if i == 1:
-            print("Cube " + str(counter+1) + " is loaded")
+            atLeast1Cube = True
+            if (verbose):
+                print("Cube " + str(counter+1) + " is loaded")
             distance = row_distance[counter]
 
             # make extender extend
             motor_row.set_position(motorStartPosition + distance)
             while motor_row.is_moving():
                 sleep(0.1)
-            print("Extender extend's to row " + str(abs(counter-5)))
+            if (verbose):
+                print("Extender extend's to row " + str(abs(counter-5)))
 
             sleep(2)  # wait before retracting
 
@@ -43,16 +47,19 @@ def pushRow(motor_row, grid, iteration):
             motor_row.set_position(motorStartPosition)
             while motor_row.is_moving():
                 sleep(0.1)
-            print("Extender retract's from row " + str(abs(counter-5)))
+            if (verbose):
+                print("Extender retract's from row " + str(abs(counter-5)))
 
             sleep(5)  # wait 5 seconds for cube to load in
 
         counter += 1
-    pushColumn(motor_column, iteration)
+    if (atLeast1Cube):  # If there is no cube then no need to push the column
+        pushColumn(motor_column, iteration)
 
 
-def pushColumn(motor_column, iteration):
-    print("Pushing wall to column " + str(abs(iteration-5)))
+def pushColumn(motor_column, iteration, verbose):
+    if (verbose):
+        print("Pushing wall to column " + str(abs(iteration-5)))
     motor_column.set_limits(dps=80)  # speed of motor
     # make sure the motor is in correct position at start!
     motorStartPosition = motor_column.get_position()
@@ -63,7 +70,8 @@ def pushColumn(motor_column, iteration):
     motor_column.set_position(motorStartPosition + distance)
     while motor_column.is_moving():
         sleep(0.1)
-    print("wall extend's to column " + str(abs(iteration-5)))
+    if (verbose):
+        print("wall extend's to column " + str(abs(iteration-5)))
 
     sleep(2)  # wait before retracting
 
@@ -71,12 +79,14 @@ def pushColumn(motor_column, iteration):
     motor_column.set_position(motorStartPosition)
     while motor_column.is_moving():
         sleep(0.1)
-    print("wall retract's from column " + str(abs(iteration-5)))
+    if (verbose):
+        print("wall retract's from column " + str(abs(iteration-5)))
 
-    print("Waiting for next row to be done")
+    if (verbose):
+        print("Waiting for next row to be done")
     sleep(3)  # wait 3 seconds then exit
 
 
-if __name__ == '__main__':
-    pushRow(motor_row, grid, 0)
-    # pushColumn(motor_row)
+# if __name__ == '__main__':
+#     pushRow(motor_row, grid, 0)
+#     # pushColumn(motor_row)
--- finalproject/project/main.py
+++ finalproject/project/main.py
@@ -5,7 +5,12 @@ import sys
 from process_input import (collect_grid_terminal_input,
                            collect_grid_touch_sensor_input, print_grid,
                            validate_grid)
-from utils.brick import TouchSensor, wait_ready_sensors
+from utils.brick import TouchSensor, Motor, wait_ready_sensors
+
+from extender import pushRow
+
+motor_row = Motor("A")  # Motor for the row pusher is in port A
+motor_column = Motor("B")  # Motor for the column pusher is in port B
 
 if __name__ == '__main__':
     # Debug mode includes terminal binary input
@@ -28,7 +33,7 @@ if __name__ == '__main__':
             print("Invalid arguments")
             exit()
     elif len(sys.argv) == 1:
-        debug = False 
+        debug = False
         verbose = False
     else:
         print("Invalid arguments")
@@ -46,7 +51,8 @@ if __name__ == '__main__':
     if debug:
         collect_grid_terminal_input(grid)
     else:
-        collect_grid_touch_sensor_input(grid, touch_sensor_0, touch_sensor_1, verbose)
+        collect_grid_touch_sensor_input(
+            grid, touch_sensor_0, touch_sensor_1, verbose)
 
     validate_grid(grid, verbose)
     print_grid(grid)
@@ -56,9 +62,5 @@ if __name__ == '__main__':
         input("Press enter to proceed...")
 
     # Run the program
-
-
-
-
-
-
+    for iteration in range(5):
+        pushRow(motor_row, motor_column, grid, iteration, verbose)
