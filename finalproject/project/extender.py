df2a7f91afd149437ce506bcc2d43450240f7f18
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-11-15 16:57:40 -0500

    move files

--- finalproject/project/extender.py
+++ finalproject/project/extender.py
@@ -106,7 +106,7 @@ def dispense_cube(motor_dispenser):
     print("start at ", motor_start_position)
     ##make one revolution of the motor
     
-    motor_dispenser.set_dps(60)  # speed of motor
+    motor_dispenser.set_dps(100)  # speed of motor
     motor_dispenser.set_position(motor_start_position + 180)
     while motor_dispenser.is_moving():
                 sleep(0.1)
@@ -114,7 +114,7 @@ def dispense_cube(motor_dispenser):
     
     sleep(2) 
     
-    motor_dispenser.set_position(motor_start_position)
+    motor_dispenser.set_position_relative(-180)
     while motor_dispenser.is_moving():
                 sleep(0.1)
     print("returning to old position at", motor_dispenser.get_position())
