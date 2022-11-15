54525dba82467c074aeefb32e24d7c89487b194a
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-11-15 17:29:55 -0500

    move files

--- finalproject/project/dispenser.py
+++ finalproject/project/dispenser.py
@@ -5,11 +5,13 @@ from time import sleep
 
 def dispense_cube(motor_dispenser):
 
-    motor_start_position = motor_dispenser.get_position()
+    motor_start_position = -150
     print("start at ", motor_start_position)
     ##make one revolution of the motor
     
-    motor_dispenser.set_dps(60)  # speed of motor
+
+    motor_dispenser.set_limits(dps=60)  # speed of motor
+    motor_dispenser.set_position(motor_start_position)
     motor_dispenser.set_position(motor_start_position + 180)
     while motor_dispenser.is_moving():
                 sleep(0.1)
