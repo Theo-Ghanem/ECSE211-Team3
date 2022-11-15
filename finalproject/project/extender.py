ff5f84b7d48c2b15a2ba66bedb1692b8915f1c43
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-11-15 17:39:04 -0500

    please work

--- finalproject/project/extender.py
+++ finalproject/project/extender.py
@@ -101,12 +101,13 @@ def push_column(motor_column, iteration, verbose):
 
 def dispense_cube(motor_dispenser):
 
-    motor_dispenser.set_limits(dps=100)  # speed limit of motor
-    motor_start_position = motor_dispenser.get_position()
-    print("start at ", motor_start_position)
+    motor_start_position = -150
+    print("start at ", motor_dispenser)
     ##make one revolution of the motor
     
+
     motor_dispenser.set_dps(60)  # speed of motor
+    motor_dispenser.set_position(motor_start_position)
     motor_dispenser.set_position(motor_start_position + 180)
     while motor_dispenser.is_moving():
                 sleep(0.1)
@@ -114,7 +115,7 @@ def dispense_cube(motor_dispenser):
     
     sleep(2) 
     
-    motor_dispenser.set_position(motor_start_position)
+    motor_dispenser.set_position_relative(-180)
     while motor_dispenser.is_moving():
                 sleep(0.1)
     print("returning to old position at", motor_dispenser.get_position())
