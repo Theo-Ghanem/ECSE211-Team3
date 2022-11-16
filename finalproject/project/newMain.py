b28f93478cd86762273bddcc3df86e9265a808aa
Ryan Reszetnik <rres@rogers.com>
2022-11-16 14:28:20 -0500

    motor adjustments

--- finalproject/project/newMain.py
+++ finalproject/project/newMain.py
@@ -12,7 +12,7 @@ preloaded_grid = [
     [0, 0, 1, 0, 0],
     [0, 0, 1, 0, 0]
 ]
-row_distances = [72,77.5, 83.5, 89, 93]#sercond pusher
+row_distances = [72,77.5, 83.5, 87.5, 93]#sercond pusher
 col_distances = [75, 90, 108, 120, 140]#first pusher
 
 
