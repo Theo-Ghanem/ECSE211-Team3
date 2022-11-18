7735991bead26cb94cf7dd1a7beb786c07fbdfaf
david <davidbreton03@gmail.com>
2022-11-18 16:08:47 -0500

    test distance

--- finalproject/project/newMain.py
+++ finalproject/project/newMain.py
@@ -12,7 +12,7 @@ preloaded_grid = [
     [0, 0, 1, 1, 0],
     [0, 0, 1, 0, 1]
 ]
-row_distances = [75.5, 80, 98, 109, 127]#second pusher
+row_distances = [75.5, 80, 96, 110, 127]#second pusher
 col_distances = [75.5, 80, 98, 100, 127]#first pusher
 
 
