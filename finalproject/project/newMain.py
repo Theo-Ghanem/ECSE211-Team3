f8c63b4621794c3c458b1d8b0be7aff71ed8af93
david <davidbreton03@gmail.com>
2022-11-18 13:51:42 -0500

    test distance

--- finalproject/project/newMain.py
+++ finalproject/project/newMain.py
@@ -13,7 +13,7 @@ preloaded_grid = [
     [0, 0, 1, 0, 1]
 ]
 row_distances = [71.5,77.5, 83.5, 87.5, 93]#second pusher
-col_distances = [75.5, 90, 106, 5, 125]#first pusher
+col_distances = [75.5, 90, 100, 110, 125]#first pusher
 
 
 def push_motor_distance(motor, distance,delay=3):
