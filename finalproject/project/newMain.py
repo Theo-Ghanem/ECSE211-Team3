2bc5f422440567ee203b09e59f3a2d64a0fc355d
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-11-19 16:21:55 -0500

    testing

--- finalproject/project/newMain.py
+++ finalproject/project/newMain.py
@@ -105,6 +105,6 @@ if __name__ == '__main__':
     # Run the program
     if verbose:
         print("\nStarting pistons...\n")
-    # print(motor_column.get_position)
-    motor_column.float_motor
+    print(motor_column.get_position())
+    motor_column.float_motor()
     # run_dispensing(grid, motor_dispenser, motor_column, motor_row)
