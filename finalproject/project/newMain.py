9ec9b6e1a0d064758a4a320705af65f729706094
Ryan Reszetnik <rres@rogers.com>
2022-11-21 00:04:38 -0500

    new motor

--- finalproject/project/newMain.py
+++ finalproject/project/newMain.py
@@ -34,7 +34,7 @@ preloaded_grid = [  # test row
 column_distances = [96, 119, 125, 148, 170]  # second pusher
 # row_distances = [70, 82, 92, 105, 128]  # first pusher #old
 # row_distances = [88, 100, 110, 128, 150]  # first pusher #new
-row_distances = [-57, -65, -75, -87, -105]  # first pusher #new
+row_distances = [-49, -59, -68, -80, -95]  # first pusher #new
 # row_distances = [-310, -430, -530, -630, -700]  # first pusher #new
 
 
