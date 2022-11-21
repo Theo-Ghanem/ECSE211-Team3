a063264e7cf1a64b3cf13d4b58b8fc79da702373
Mathieu Geoffroy <mathieu.geoffroy@mail.mcgill.ca>
2022-11-20 23:10:26 -0500

    motor tests

--- finalproject/project/newMain.py
+++ finalproject/project/newMain.py
@@ -31,10 +31,10 @@ preloaded_grid = [  # test row
 ]
 
 
-column_distances = [102, 119, 134, 150, 170]  # second pusher
+column_distances = [102, 119, 132, 148, 170]  # second pusher
 # row_distances = [70, 82, 92, 105, 128]  # first pusher #old
 # row_distances = [88, 100, 110, 128, 150]  # first pusher #new
-row_distances = [-55, -65, -75, -90, -105]  # first pusher #new
+row_distances = [-55, -65, -75, -87, -105]  # first pusher #new
 
 
 def push_motor_distance(motor, distance, delay=3):
