5c46355db9af1e6df901c9116702dccf891adcd6
Ryan Reszetnik <rres@rogers.com>
2022-11-22 16:32:06 -0500

    motor test

--- finalproject/project/newMain.py
+++ finalproject/project/newMain.py
@@ -35,7 +35,7 @@ column_distances = [96, 119, 125, 148, 170]  # second pusher
 # row_distances = [70, 82, 92, 105, 128]  # first pusher #old
 # row_distances = [88, 100, 110, 128, 150]  # first pusher #new
 # row_distances = [-40, -50, -64, -72, -85]  # first pusher #new
-row_distances = [-310, -430, -530, -630, -700]  # first pusher #new
+row_distances = [-312, -430, -530, -635, -730]  # first pusher #new
 
 
 def push_motor_distance(motor, distance, delay=3):
