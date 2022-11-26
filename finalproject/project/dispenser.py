19d6af5873c142254616c4dd14f1b1bc3e09d9af
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-11-26 16:39:53 -0500

    testing dispenser

--- finalproject/project/dispenser.py
+++ finalproject/project/dispenser.py
@@ -1,26 +1,22 @@
-#!/usr/bin/env python3
+
 
 from utils.brick import Motor
 from time import sleep
 
-def dispense_cube(motor_dispenser):
+def dispense_cube(motor):
+    motor.set_limits(dps=0)
+    motor.set_position_relative(170)
+    while motor.is_moving():
+        sleep(0.1)
+    # print("push should be done")
+    sleep(1.25)
+    motor.set_limits(dps=250)
+    motor.set_position_relative(-170)
+    while motor.is_moving():
+        sleep(0.1)
+    # print("retraction should be done",-distance)
+    sleep(1.25)
 
-    motor_start_position = -150
-    print("start at ", motor_start_position)
-    ##make one revolution of the motor
-    
-
-    motor_dispenser.set_dps(60)  # speed of motor
-    motor_dispenser.set_position(motor_start_position)
-    motor_dispenser.set_position(motor_start_position + 180)
-    while motor_dispenser.is_moving():
-                sleep(0.1)
-    print("pushing cube out at ", motor_dispenser.get_position())
-    
-    sleep(2) 
-    
-    motor_dispenser.set_position_relative(-180)
-    while motor_dispenser.is_moving():
-                sleep(0.1)
-    print("returning to old position at", motor_dispenser.get_position())
-    motor_dispenser.set_dps(0)  # speed of motor
+motor_dispenser = Motor("C")  # Motor for the dispenser is in port C
+motor_dispenser.set_limits(dps=60)  # speed of motor
+dispense_cube(motor_dispenser)
