02c196176645732dab19f7b386d8d32d6e652e86
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-11-26 16:47:07 -0500

    testing dispenser

--- finalproject/project/dispenser.py
+++ finalproject/project/dispenser.py
@@ -4,18 +4,18 @@ from utils.brick import Motor
 from time import sleep
 
 def dispense_cube(motor):
-    motor.set_limits(dps=60)
+    motor.set_limits(dps=80)
     motor.set_position_relative(170)
     while motor.is_moving():
         sleep(0.1)
     # print("push should be done")
-    sleep(1.25)
-    motor.set_limits(dps=60)
+    sleep(1.5)
+    motor.set_limits(dps=80)
     motor.set_position_relative(-170)
     while motor.is_moving():
         sleep(0.1)
     # print("retraction should be done",-distance)
-    sleep(1.25)
+    sleep(1.5)
 
 motor_dispenser = Motor("C")  # Motor for the dispenser is in port C
 motor_dispenser.set_limits(dps=60)  # speed of motor
