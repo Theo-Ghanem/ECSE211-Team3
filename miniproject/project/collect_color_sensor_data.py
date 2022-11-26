fd3d4b53af1c2c564ba5dd2abc2118ce6a219c72
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-11-26 18:09:28 -0500

    testing dispenser

--- miniproject/project/collect_color_sensor_data.py
+++ miniproject/project/collect_color_sensor_data.py
@@ -14,22 +14,22 @@ COLOR_SENSOR_DATA_FILE = "../data_analysis/color_sensor.csv"
 SENSOR_POLL = 0.05
 
 # complete this based on your hardware setup
-sw = EV3ColorSensor(2)
-touch = TouchSensor(1)
+sw = EV3ColorSensor(1)
+touch = TouchSensor(3)
 
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
 
 
 def collect_color_sensor_data():
     try:
-        f = open(COLOR_SENSOR_DATA_FILE, "w")
+        # f = open(COLOR_SENSOR_DATA_FILE, "w")
         running = False
         while True:
             sleep(0.01)
             if touch.is_pressed() and not running:
                 print("pressed")
                 sd = sw.get_value()
-                f.write('{:d},{:d},{:d},{:d}\n'.format(sd[0],sd[1],sd[2],sd[3]))
+                print('{:d},{:d},{:d},{:d}\n'.format(sd[0],sd[1],sd[2],sd[3]))
                 sleep(SENSOR_POLL)
                 running = True
             elif not touch.is_pressed() :
@@ -41,7 +41,7 @@ def collect_color_sensor_data():
         pass
     finally:
         print("Done collecting color sensor")
-        f.close()
+        # f.close()
         reset_brick() # Turn off everything on the brick's hardware, and reset it
         exit()
 
