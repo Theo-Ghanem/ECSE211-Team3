a3a3ea2b2aa262552a9a4fc29aee1ed76a7a78f5
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-11-26 17:25:45 -0500

    testing dispenser

--- finalproject/project/dispenser.py
+++ finalproject/project/dispenser.py
@@ -6,17 +6,18 @@ from time import sleep
 def dispense_cube(motor):
     motor.set_limits(dps=80)
     motor.set_position_relative(170)
-    while motor.is_moving():
-        sleep(0.1)
+    # while motor.is_moving():
+    #     sleep(0.1)
     # print("push should be done")
     sleep(1.5)
-    motor.set_limits(dps=80)
+    # motor.set_limits(dps=80) 
     motor.set_position_relative(-170)
-    while motor.is_moving():
-        sleep(0.1)
+    # while motor.is_moving():
+    #     sleep(0.1)
     # print("retraction should be done",-distance)
     sleep(1.5)
+    motor.set_power(0)
 
 motor_dispenser = Motor("C")  # Motor for the dispenser is in port C
-motor_dispenser.set_limits(dps=60)  # speed of motor
+motor_dispenser.set_limits(dps=80)  # speed of motor
 dispense_cube(motor_dispenser)
