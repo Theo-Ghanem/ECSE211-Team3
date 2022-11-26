0183302c11d559a1483987e8b0ac7fea1c49189e
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-11-26 18:02:40 -0500

    testing dispenser

--- miniproject/project/collect_us_sensor_data.py
+++ miniproject/project/collect_us_sensor_data.py
@@ -16,8 +16,8 @@ SOUND = sound.Sound(duration=0.3, pitch="A4", volume=60)
 
 print("Program start.\nWaiting for sensors to turn on...")
 
-TOUCH_SENSOR = TouchSensor(1)
-US_SENSOR = EV3UltrasonicSensor(2)
+TOUCH_SENSOR = TouchSensor(3)
+US_SENSOR = EV3UltrasonicSensor(1)
 
 
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
@@ -27,7 +27,7 @@ print("Done waiting.")
 def collect_continuous_us_data():
     "Collect continuous data from the ultrasonic sensor between two button presses."
     try:
-        output_file = open(US_SENSOR_DATA_FILE, "w")
+        # output_file = open(US_SENSOR_DATA_FILE, "w")
         while not TOUCH_SENSOR.is_pressed():
             pass  # do nothing while waiting for first button press
         print("Touch sensor pressed")
@@ -37,13 +37,13 @@ def collect_continuous_us_data():
             us_data = US_SENSOR.get_value()  # Float value in centimeters 0, capped to 255 cm
             if us_data is not None: # If None is given, then data collection failed that time
                 print(us_data)
-                output_file.write(f"{us_data}\n")
+                # output_file.write(f"{us_data}\n")
             sleep(DELAY_SEC)
     except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
         pass
     finally:
         print("Done collecting US distance samples")
-        output_file.close()
+        # output_file.close()
         reset_brick() # Turn off everything on the brick's hardware, and reset it
         exit()
 
