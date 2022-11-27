163fb18811fafbb0a43a10216141f68f9aa056a6
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-11-27 00:06:58 -0500

    resetting row dist

--- finalproject/project/newMain.py
+++ finalproject/project/newMain.py
@@ -44,7 +44,7 @@ preloaded_grid = [  # test row
 
 
 column_distances = [120, 138, 158, 175, 205]  # second pusher
-row_distances = [298, 400, 528, 635, 750]  # first pusher #new
+row_distances = [300, 400, 530, 635, 745]  # first pusher #new
 tone1 = Sound(duration=0.5, volume=90, pitch="C4")
 tone2 = Sound(duration=0.5, volume=90, pitch="D4")
 
