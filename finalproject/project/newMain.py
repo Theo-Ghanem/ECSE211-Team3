1d87a4fb1a91f0cfbd534a3ac45e0ad664f29f62
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-11-26 20:59:34 -0500

    recalibrate distances

--- finalproject/project/newMain.py
+++ finalproject/project/newMain.py
@@ -35,8 +35,8 @@ preloaded_grid = [  # test column
 # ]
 
 
-column_distances = [118, 130, 140, 159, 200]  # second pusher
-row_distances = [300, 430, 530, 635, 740]  # first pusher #new
+column_distances = [90, 123, 145, 160, 200]  # second pusher
+row_distances = [300, 400, 530, 635, 740]  # first pusher #new
 tone1 = Sound(duration=0.5, volume=90, pitch="C4")
 tone2 = Sound(duration=0.5, volume=90, pitch="D4")
 
