47068bd8fc2f98cdd757a597d08d7370137b5cc4
Ryan Reszetnik <rres@rogers.com>
2022-11-27 22:01:32 -0500

    updated motor testing

--- finalproject/project/first_pusher_test.py
+++ finalproject/project/first_pusher_test.py
@@ -5,6 +5,13 @@ from utils.brick import Motor
 
 distances = [300, 400, 530, 635, 745]
 
+def dispense_cube(motor):
+    motor.set_limits(dps=100)
+    motor.set_position_relative(160)
+    sleep(1.75)
+    motor.set_position_relative(-160)
+    sleep(1.75)
+    motor.set_power(0)
 
 
 def push_motor_distance(motor, distance, delay=3):
@@ -21,10 +28,12 @@ def push_motor_distance(motor, distance, delay=3):
     sleep(delay)
 
 if __name__ == "__main__":
+    motor_dispenser = Motor("C")
     motor_row = Motor("B")  # Motor for the row pusher is in port B
     motor_row.set_limits(dps=360)
     rev_distances = distances
     rev_distances.reverse()
     for distance in rev_distances:
+        dispense_cube(motor_dispenser)
         push_motor_distance(motor_row,distance)
     
